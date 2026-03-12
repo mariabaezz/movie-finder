4e11d54760fd56d17a71cd144de9dd259abb0707
María Báez <baezmarreromaria9@gamil.com>
2026-03-12 18:51:42 +0000

    CLI menu

--- main.py
+++ main.py
@@ -21,16 +21,32 @@ def search_movie(title):
             "IMDb_Rating": response['imdbRating']
         }
 
-
+def menu():
+    while True:
+        print("Welcome to movie search!")
+        print("1. Search for a movie")
+        print("2. Exit")
+        choice = int(input("Enter your choice: "))
+        if choice > 2 or choice < 1:
+            print("Invalid choice. Please try again.")
+            return menu()
+        if choice == 1:
+            title = input("Enter movie title: ")
+            result = search_movie(title)
+            if result is None:
+                print("Movie not found.")
+            else:
+                print(f"Title: {result['Title']}")
+                print(f"Year: {result['Year']}")
+                print(f"Genre: {result['Genre']}")
+                print(f"Director: {result['Director']}")
+                print(f"IMDb Rating: {result['IMDb_Rating']}")
+            menu()
+        if choice == 2:
+            break
+        
+        print("Goodbye!")
+        exit()
 
 if __name__ == "__main__":
-    title = input("Enter movie title: ")
-    result = search_movie(title)
-    if result is None:
-        print("Movie not found.")
-    else:
-        print(f"Title: {result['Title']}")
-        print(f"Year: {result['Year']}")
-        print(f"Genre: {result['Genre']}")
-        print(f"Director: {result['Director']}")
-        print(f"IMDb Rating: {result['IMDb_Rating']}")
+    menu()
